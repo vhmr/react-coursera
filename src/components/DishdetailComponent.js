19f34d92e1e0b3e4f38cfbd78802dfe466d71cd0
Victor Montoya CTO <vmontoya@eyss-dev.com>
2018-10-15 10:37:10 -0400

    Fetch from Serve

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Card,CardImg,CardText,CardBody,CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Col, Row} from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 import { Link } from 'react-router-dom';
 const required = (val) => val && val.length;
@@ -126,7 +128,7 @@ class CommentForm extends Component {
     function RenderDish({dish}) {
         return (
             <Card>
-                <CardImg width="100%" src = { dish.image } alt = { dish.name } />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name }</CardTitle>
                     <CardText>{ dish.description }</CardText>
